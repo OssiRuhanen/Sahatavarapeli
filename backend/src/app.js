779acd4737dcbac96d5b19588542778be698c15e
Pussmarr <hodari86@hotmail.com>
2020-10-08 17:14:03 +0300

    Added post for posting JSON objects

--- backend/src/app.js
+++ backend/src/app.js
@@ -35,11 +35,31 @@ app.get('/', function(req, res, next) {
     console.log(randomObject)
   });
 
-  // get /image for testing purposes
+// get /image for testing purposes
 app.get('/image', function(req, res, next) {
     var randomObject = values[parseInt(Math.random() * values.length)]
     res.sendFile(filepath+randomObject.Image)
   })
+
+// post request for adding new content to lankut.json
+// Tää varmaan pitäis tehdä promisella että saa noi errorit mukaan
+// varmaan kanssa jotain chekkejä että toi body on oikein
+app.post('/', function(req, res, next) {
+  fs.readFile(pathTowoodLibrary, (error, data) =>  {
+    var json = JSON.parse(data)
+    json.push(req.body)
+    // write JSON string to a file
+    fs.writeFile("./"+pathTowoodLibrary, JSON.stringify(json), (err) => {
+      if (err) {
+          console.log("err")
+      }
+      else {
+        console.log("JSON data is saved.")
+      }
+    }); 
+  }) 
+  res.json(req.body)
+})
 
 app.listen(port, () => {
   console.log(`Sahapeli app listening at http://localhost:${port}`)
