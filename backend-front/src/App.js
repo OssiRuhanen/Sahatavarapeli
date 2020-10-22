36dd3eeff7126a9a2b096e8a1f191b8176fc6f44
Simo Wesa <simo.wesa@gmail.com>
2020-10-22 12:09:40 +0300

    backend-front: added global variable
    Right now there is only one, however it can be easily expanded.
    Just don't put secrets in here.

--- backend-front/src/App.js
+++ backend-front/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import {backend} from './components/variables';
 
 function App() {
   return (
     <div className="App">
-      
+
     </div>
   );
 }
--- backend-front/src/components/variables.js
+++ backend-front/src/components/variables.js
@@ -1,3 +1,9 @@
+//File for setting global variables
+//Usage
+//import {backend} from './components/variables'
 export const backend = {
     URL: "localhost:3001"
 }
+
+
+export default backend
