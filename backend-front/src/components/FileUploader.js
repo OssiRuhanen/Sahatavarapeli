ace8d0d052a8ce802e57293382c6e1cba9c512f1
cardbaron <simo.wesa@gmail.com>
2020-12-03 14:33:56 +0200

    Update FileUploader.js
    Added:
    File[] upload
    file input filter

--- backend-front/src/components/FileUploader.js
+++ backend-front/src/components/FileUploader.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import axios from 'axios'
-import {Form, FormGroup, Card} from 'react-bootstrap'
+import {Form, FormGroup, Card, Button} from 'react-bootstrap'
 import {backend} from './variables'
 
-const FileUploader = () => {
+const FileUploader = (props) => {
+
+    const [files, setFiles] = useState([])
 
     function uploadFile(file){
         let fd = new FormData();
@@ -29,17 +31,51 @@ const FileUploader = () => {
                 return false
             })
     }
-
-    async function fileHandler(e){
-        console.log("filehandler",e.target.files[0]);
-        let file = e.target.files[0]
-        if(await uploadFile(file)){
-            //handle success
-            console.log("Success")
+    async function uploadFiles(files){ //file[]
+        console.log("Starting upload")
+        let retry = 0;
+        while(files.length > 0 && retry < 3){
+            if(await uploadFile(files[0])){
+                retry = 0
+                files.shift(); //removes first element
+            }
+            else{
+                //console.log("Retry file")
+                retry++;
+            }
+        }
+        if(retry >= 3){
+            console.log("Error","Failed to upload a file after multiple attempts")
         }
         else{
-            //handle error
-            console.log("Error")
+            console.log("Success","upload is completed")
+        }
+    }
+
+    async function fileHandler(e){
+        let file = e.target.files[0]
+        if(!addFile(file)) console.log("Error", "Wrong File Type");
+        // if(await uploadFile(file)){
+        //     //handle success
+        //     console.log("Success")
+        // }
+        // else{
+        //     //handle error
+        //     console.log("Error")
+        // }
+
+    }
+
+    function addFile(file){
+        console.log("filehandler",file);
+        const fileTypes = /jpeg|jpg|png|gif/
+        const isOfType = fileTypes.test(file.name)
+        if(isOfType){
+            setFiles(files.concat(file));
+            return true;
+        }
+        else{
+            return false;
         }
     }
     return (
@@ -47,11 +83,12 @@ const FileUploader = () => {
         <Card>
             <Form>
                 <FormGroup>
-                    <Form.File onChange={fileHandler} label="Image uploader"/>
+                    <Form.File onChange={(e)=>{fileHandler(e); e.target.value = null}} label="Image uploader"/>
                 </FormGroup>
             </Form>
         </Card>
-         
+        <Button onClick={()=>{console.log(JSON.stringify(files))}}>log files</Button>
+        <Button onClick={()=>{uploadFiles(files)}}>Upload files</Button>
         </>
     )
 }
